f299b36fc1527485e79b715d54a7b3005c7ad680
111RED111 <55107246+111RED111@users.noreply.github.com>
2021-04-05 18:48:28 +0300

    1

--- Diplom111/Form1.Designer.cs
+++ Diplom111/Form1.Designer.cs
@@ -51,41 +51,41 @@ namespace Diplom111
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 46);
+            this.button1.Location = new System.Drawing.Point(34, 34);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 37);
+            this.button1.Size = new System.Drawing.Size(139, 61);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Старт";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(220, 46);
+            this.button2.Location = new System.Drawing.Point(205, 34);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 37);
+            this.button2.Size = new System.Drawing.Size(139, 61);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Стоп";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(362, 46);
+            this.button3.Location = new System.Drawing.Point(562, 34);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(106, 37);
+            this.button3.Size = new System.Drawing.Size(139, 61);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Конвертировать";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(507, 46);
+            this.button4.Location = new System.Drawing.Point(373, 34);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(106, 37);
+            this.button4.Size = new System.Drawing.Size(139, 61);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "Нарисовать";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -102,12 +102,12 @@ namespace Diplom111
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1235, 740);
+            this.ClientSize = new System.Drawing.Size(1262, 740);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
